c08b0e6797aa40eeed47d7cbb4a225b1433e0b21
Pedro Marins Baier da Silva <pedromarins2023@gmail.com>
2026-06-17 11:55:32 -0300

    aula 04

--- aula 04/EX01.js
+++ aula 04/EX01.js
@@ -1,18 +1,34 @@
-const cod = 123;
 
 
 
 
-if (cod == 1) {
-console.log("Voce escolheu incluir");
-} else if (cod == 2) {
-    console.log("Voce escolheu Alterar");
-} else if ( cod == 3) {
-    console.log("Voce escolheu Excluir");
-} else if (cod == 4) {
-    console.log("Voce escolheu Pesquisar");
 
-} else if ( cod == 5) { 
-    console.log("Voce escolheu Sair");
+
+console.log("código - Opção");
+console.log(" 1 - Incluir");
+console.log(" 2 - Alterar");
+console.log(" 3 - Excluir");
+console.log(" 4 - Pesquisar");
+console.log(" 5 - Sair");
+
+let op = 5;
+
+switch (op) {
+  case 1:
+    console.log("Você escolheu incluir");
+    break;
+  case 2:
+    console.log("Você escolheu alterar");
+    break;
+  case 3:
+    console.log("Você escolheu excluir");
+    break;
+  case 4:
+    console.log("Você escolheu pesquisar");
+    break;
+  case 5:
+    console.log("Você escolheu sair");
+    break;
+  default:
+    console.log("Opção inválida!");
 }
-
